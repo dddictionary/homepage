f70c2c2a0562846c5096a842e7932374fcdb8df0
dddictionary <abrarhabib285@gmail.com>
2023-09-29 16:27:04 -0400

    ready, steady, go!

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio
+This is a repository containinghte code for my portfolio website.
+It's hosted at [placeholder](www.example.com)
---
+++ script.js
@@ -0,0 +1,31 @@
+import anime from "./node_modules/animejs/lib/anime.es.js"
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+let myanime = anime.timeline({autoplay:false })
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  });
+
+myanime.play();
